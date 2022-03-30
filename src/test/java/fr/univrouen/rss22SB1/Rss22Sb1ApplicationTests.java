c94ce7b412e3259666225d41b6de9641e1273427
elhamra <melhamra98@gmail.com>
2022-03-30 10:13:47 +0200

    premier commit

---
+++ src/test/java/fr/univrouen/rss22SB1/Rss22Sb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.univrouen.rss22SB1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rss22Sb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
